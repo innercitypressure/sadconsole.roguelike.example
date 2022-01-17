776cb5d2a31b961b7da0dec4676738fbbc73dcda
Casey Smith <casey1099etc@gmail.com>
2022-01-17 17:28:22 -0600

    Init

---
+++ Roguelike.Models/Floor.cs
@@ -0,0 +1,6 @@
+namespace Roguelike.Models;
+
+public class Floor
+{
+    
+}
---
+++ Roguelike.UI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SadConsole;
+using SadConsole.Input;
+using SadRogue.Primitives;
+using Console = SadConsole.Console;
+
+namespace MyProject
+{
+    class Program
+    {
+
+        public const int Width = 80;
+        public const int Height = 25;
+
+        static void Main(string[] args)
+        {
+            // Setup the engine and create the main window.
+            SadConsole.Game.Create(Width, Height);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.Instance.OnStart = Init;
+                        
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        private static void Init()
+        {
+            // Any startup code for your game. We will use an example console for now
+            var startingConsole = Game.Instance.StartingConsole;
+
+            startingConsole.FillWithRandomGarbage(SadConsole.Game.Instance.StartingConsole.Font);
+            startingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, Mirror.None);
+            startingConsole.Print(4, 4, "Hello from SadConsole");
+            startingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateBorder(new ColoredGlyph(Color.Violet, Color.Black, 176)));
+        }
+    }
+}
